26aae2b17435f81709151613899953fbee350390
Anthony Pileggi <adpileggi@gmail.com>
2024-03-09 16:39:47 -0500

    add server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Cfh8189Pil!',
+        // make database
+        database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database`)
+);
+
+// add department route
+
+// add role route 
+
+// add employee route
+db.query(``)
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
