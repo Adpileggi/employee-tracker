d37ab3ba6657a429b2626d2d142720a329de55d3
Anthony Pileggi <adpileggi@gmail.com>
2024-03-11 10:27:21 -0400

    deleted unused files from file structure

--- db/query.sql
+++
@@ -1,5 +0,0 @@
--- request to see all department names and ID's
-
--- request to see all role.title, salary, role id, dept name
-
--- request to see employee id, first name, last name, job titles, salary and manager
